c1986db28c70cc8c0eba2fa2248aad7510f2d25b
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-29 15:45:10 +0500

    component folder add

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
