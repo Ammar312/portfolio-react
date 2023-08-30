1fb6cbc00c884427fb4b77b129509a5ed3cc24af
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-30 13:01:44 +0500

    working on main banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import image from "../assets/avatar.svg";
+import { FaGithub, FaLinkedin, FaYoutube } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   return (
     <div className="section" id="home">
-      Banner
+      <div className=" container mx-auto">
+        <div>
+          {/* text */}
+          <div>Text</div>
+          <div>
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
