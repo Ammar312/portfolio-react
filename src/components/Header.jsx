cde7b2c229b6c2b89ec256909b8fbfa3e37d1ed4
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-30 11:48:42 +0500

    working on banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import logo from "../assets/logo.svg";
 const Header = () => {
   return (
     <header className="py-8">
-      <div className=" container mx-auto">
-        <div className=" flex justify-between items-center">
+      <div className=" flex justify-center">
+        <div className=" container flex justify-between items-center min-w-[350px] max-w-[700px]">
           <a href="#">
             <img src={logo} alt="" />
           </a>
