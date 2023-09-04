abeb5d373ae34bb3fd32621c8b436f41a20207bd
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-05 01:24:00 +0500

    workin on work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,7 +7,7 @@ const Work = () => {
   return (
     <section className="section" id="work">
       <div className=" container mx-auto">
-        <div>
+        <div className=" flex flex-col lg:flex-row gap-x-10">
           <div className=" flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
             <div>
               <h2 className=" font-orbitron text-[27px] mb-2 tracking-[10%] uppercase leading-tight text-accent">
@@ -22,14 +22,20 @@ const Work = () => {
             </div>
             <div className=" group relative border-white/50 border-2 overflow-hidden rounded-xl">
               <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
-              <img src="" alt="" />
-              <div>UI/UX Design</div>
-              <div>
-                <span>Project Title</span>
+              <img
+                src={img1}
+                alt="work"
+                className=" group-hover:scale-125 transition-all duration-500"
+              />
+              <div className=" absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className=" text-gradient">UI/UX Design</span>
+              </div>
+              <div className=" absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className=" text-2xl text-gradient">Project Title</span>
               </div>
             </div>
           </div>
-          <div>2</div>
+          <div className=" flex-1 ">2</div>
         </div>
       </div>
     </section>
