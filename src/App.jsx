c42e7dcd19a3cac0614b37bce0c34e1a5b9e332d
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-05 08:40:55 +0500

    working on contact

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Services />
       <Work />
       <Contact />
-      <div className=" h-[1220px]"></div>
+      <div className="h-[4000px]"></div>
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Contact = () => {
   return (
-    <div className="section" id="contact">
-      Contact
-    </div>
+    <section className=" py-16 lg:section" id="contact">
+      <div className=" container mx-auto">
+        <div className=" flex flex-col lg:flex-row">
+          {/* text */}
+          <div>
+            <div className=" flex-1">
+              <h4 className=" text-xl uppercase text-accent font-medium mb-2 tracking-wide">
+                Get in touch
+              </h4>
+              <h2 className=" text-[45px] lg:text-[90px] uppercase leading-none mb-12">
+                Let's Work <br /> Together
+              </h2>
+            </div>
+          </div>
+          {/* form */}
+          <form className=" flex-1">
+            <input
+              type="text"
+              className=" bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent"
+            />
+          </form>
+        </div>
+      </div>
+    </section>
   );
 };
 
