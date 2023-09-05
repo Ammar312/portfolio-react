7ff9ba66a94c62087364f5adfd154a7e0a96028f
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-05 13:05:22 +0500

    contact ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,24 +6,35 @@ const Contact = () => {
   return (
     <section className=" py-16 lg:section" id="contact">
       <div className=" container mx-auto">
-        <div className=" flex flex-col lg:flex-row">
+        <div className=" flex flex-col lg:flex-row gap-x-10">
           {/* text */}
           <div>
             <div className=" flex-1">
               <h4 className=" text-xl uppercase text-accent font-medium mb-2 tracking-wide">
                 Get in touch
               </h4>
-              <h2 className=" text-[45px] lg:text-[90px] uppercase leading-none mb-12">
-                Let's Work <br /> Together
+              <h2 className=" text-[45px] lg:text-[75px] leading-none mb-12">
+                Let's work <br /> together!
               </h2>
             </div>
           </div>
           {/* form */}
-          <form className=" flex-1">
+          <form className=" flex-1 flex border pb-24 p-6 items-start rounded-2xl flex-col gap-y-6">
             <input
               type="text"
-              className=" bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent"
+              className=" bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              placeholder="Your Name"
             />
+            <input
+              type="text"
+              className=" bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              placeholder="Your Email"
+            />
+            <textarea
+              className=" bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
+              placeholder="Your Message"
+            ></textarea>
+            <button className=" btn btn-lg">Send Message</button>
           </form>
         </div>
       </div>
