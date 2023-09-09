ebfbb86824da7d571bd74092d3a434906ef4284e
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-09 20:16:57 +0500

    logo change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,19 @@ const Header = () => {
     <header className="py-8">
       <div className=" flex justify-center">
         <div className=" container flex justify-between items-center min-w-[300px] max-w-[990px] px-4">
-          <a href="#">
+          {/* <a href="#">
             <img src={logo} alt="" />
-          </a>
+          </a> */}
+          <div className=" w-[111px] h-[48] text-2xl font-extrabold leading-[23px] font-orbitron">
+            <div className=" text-gradient">AMMAR</div>
+            MUSTAFA
+          </div>
           <button className=" btn btn-sm">Work With Me</button>
         </div>
       </div>
     </header>
   );
 };
+// border-radius: 49% 51% 69% 31% / 51% 37% 63% 49%;
 
 export default Header;
