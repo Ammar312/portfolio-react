3cdc1f8cdaac7423964b7601f10fa729c5407bd8
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-11 11:48:15 +0500

    working on projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section className="section border-2" id="projects">
+      <div className=" border-2 border-green-500">
+        <h1 className=" text-3xl text-gradient border">MY PROJECTS</h1>
+        <div className="border border-red-300"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
