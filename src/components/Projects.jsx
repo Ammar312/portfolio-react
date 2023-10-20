3c13c7a2a84e235b1333369b20c1644998bfed8c
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-21 01:31:37 +0500

    add animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { BsArrowUpRight } from "react-icons/bs";
 import constant from "../constants/Constant";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Projects = () => {
   return (
     <section
-      className="py-8 lg:py-9 px-8 lg:h-screen flex border-2 overflow-hidden"
+      className="py-8 lg:py-9 px-8 lg:h-screen flex  overflow-hidden"
       id="projects"
     >
       <div className=" h-full max-w-full overflow-hidden ">
         <h1 className=" text-3xl text-gradient mb-8">MY PROJECTS</h1>
-        <div className=" flex flex-1 gap-4 flex-wrap justify-center gap-x-8 h-full overflow-y-auto scrollbar">
+        <div className=" flex flex-1 gap-4 flex-wrap justify-center gap-x-12 gap-y-16 h-full overflow-y-auto scrollbar ">
           {constant.map((card, index) => (
-            <div
-              className="group relative border-white/50 border-2 overflow-hidden rounded-xl max-w-[550px]"
+            <motion.div
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="group relative border-white/50 border-2 overflow-hidden rounded-xl max-w-[550px] "
               key={index}
             >
               <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
@@ -37,7 +43,7 @@ const Projects = () => {
               <div className=" absolute -bottom-full left-8 group-hover:bottom-4 transition-all duration-700 z-50 ">
                 <span className=" text-white">{card.description}</span>
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
