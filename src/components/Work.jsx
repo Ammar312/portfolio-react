494e236e0e7ac279b92c77da4abb80b3238a2da4
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-28 07:13:22 +0500

    add another page

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 import img1 from "../assets/portfolio.png";
+import social from "../assets/socialmedia.png";
 
 const Work = () => {
   return (
@@ -17,8 +18,8 @@ const Work = () => {
           >
             <div>
               <h2 className=" font-orbitron text-[27px] mb-2 tracking-[10%] uppercase leading-tight text-accent">
-                My Latest <br />
-                Work
+                Upcoming <br />
+                Projects
               </h2>
               <p className="max-w-sm mb-8">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
@@ -51,15 +52,20 @@ const Work = () => {
             <div className=" group relative border-white/50 border-2 overflow-hidden rounded-xl">
               <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               <img
-                src={img1}
+                src={social}
                 alt="work"
                 className=" group-hover:scale-125 transition-all duration-500"
               />
+              <div className=" absolute -bottom-full left-56 group-hover:bottom-40 transition-all duration-500 z-50">
+                <span className=" text-gradient text-3xl">Coming Soon...</span>
+              </div>
               <div className=" absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
-                <span className=" text-gradient">UI/UX Design</span>
+                <span className=" text-gradient">MERN STACK</span>
               </div>
               <div className=" absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
-                <span className=" text-2xl text-gradient">Project Title</span>
+                <span className=" text-2xl text-gradient">
+                  Social Media App
+                </span>
               </div>
             </div>
             <div className=" group relative border-white/50 border-2 overflow-hidden rounded-xl">
